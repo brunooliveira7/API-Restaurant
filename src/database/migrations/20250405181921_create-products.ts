b1728a8d498606d9dbd7b1ef998ebbbb1313f449
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-04-05 15:53:36 -0300

    configures the migration by creating the table and its columns, and creates the database.

--- src/database/migrations/20250405181921_create-products.ts
+++ src/database/migrations/20250405181921_create-products.ts
@@ -1,10 +1,18 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  //cria tabela de produtos
+  await knex.schema.createTable("products", (table) => {
+    //cria colunas - a primeira coluna é id com primary key
+    table.increments("id").primary();
+    table.text("name").notNullable();
+    table.decimal("price").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
 }
 
-
+//desfazer a migration
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("products");
 }
-
