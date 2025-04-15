e1174eca673c135d52b3cb5d5b8eb2e258dce7b5
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-04-14 21:16:34 -0300

    creating the typing and method of opening the table session

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,8 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { knex } from "@/database/knex";  
+import { z } from "zod";
 
 class TableSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
+      const BodySchema = z.object({
+        table_id: z.number(),
+      });
+
+      const { table_id } = BodySchema.parse(request.body);
+
+      await knex<TableSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      });
+
       return response.status(201).json();
     } catch (error) {
       next(error);
