dc12414fd3941c473cf62c32f42ac67da574b570
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-04-16 21:18:32 -0300

    redoing the opening of tables

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
-import { knex } from "@/database/knex";  
+import { AppError } from "@/utils/AppError";
+import { knex } from "@/database/knex";
 import { z } from "zod";
 
 class TableSessionsController {
@@ -11,6 +12,18 @@ class TableSessionsController {
 
       const { table_id } = BodySchema.parse(request.body);
 
+      //verificar se a mesa já está aberta
+      const session = await knex<TableSessionsRepository>("tables_sessions")
+        .where({ table_id })
+        .orderBy("opened_at", "desc")
+        .first();
+
+        //se a mesa está aberta, não pode abrir outra sessão
+        if (session && !session.closed_at) {
+          throw new AppError("Mesa já está aberta");
+          };
+        
+      //abrir sessão de uma mesa
       await knex<TableSessionsRepository>("tables_sessions").insert({
         table_id,
         opened_at: knex.fn.now(),
