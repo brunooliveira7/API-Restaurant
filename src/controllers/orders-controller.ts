868c1523bfa4a345a1b768486103e4694ea6c120
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-04-21 10:48:47 -0300

    validations for adding items to a table and products session

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { knex } from "@/database/knex";
 import { z } from "zod";
 
 class OrdersController {
@@ -15,8 +17,33 @@ class OrdersController {
       const { table_session_id, product_id, quantity } = bodySchema.parse(
         request.body
       );
-      
-      return response.status(201).json();
+
+      //se tem a session - carrega
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first();
+
+      //se não existir session
+      if (!session) {
+        throw new AppError("Session table not found");
+      }
+
+      //se tem data de fechamento - sessão fechada, não pode criar mais pedidos
+      if (session.closed_at) {
+        throw new AppError("This table is closed");
+      }
+
+      //se tem produto - carrega
+      const product = await knex<ProductsRepository>("products")
+        .where({ id: product_id })
+        .first();
+
+      //se não existir produto
+      if (!product) {
+        throw new AppError("Product not found");
+      }
+
+      return response.status(201).json(product);
     } catch (error) {
       next(error);
     }
